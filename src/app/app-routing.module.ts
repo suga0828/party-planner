b352f162e88f4a46093847b550dec50ef8cc4c3c
Alexander Sandoval <alexander.sf0828@gmail.com>
2020-02-15 14:33:44 -0500

    Create people module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'people' },
+  { path: 'people', loadChildren: () => import('./people/people.module').then(m => m.PeopleModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
